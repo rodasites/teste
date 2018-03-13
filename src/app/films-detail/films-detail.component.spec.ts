fb250f18b3851714900362d9ee6342de99507e5e
Rômulo Braun <romulo.braun@southsystem.com.br>
2018-03-13 14:31:07 -0300

    Commit

--- src/app/films-detail/films-detail.component.spec.ts
+++ src/app/films-detail/films-detail.component.spec.ts
@@ -19,7 +19,5 @@ describe('FilmsDetailComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+
 });
--- src/app/films-page/films-page.component.spec.ts
+++ src/app/films-page/films-page.component.spec.ts
@@ -19,7 +19,5 @@ describe('FilmsPageComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+
 });
